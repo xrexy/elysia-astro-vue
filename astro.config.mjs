61c0034f42652ac14f82a44be82005eef32dfc5f
xrexy <rexydoesimportantthings@gmail.com>
2024-03-19 15:58:54 +0200

    add shadcn-vue and tailwi9nd

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
 import { defineConfig } from 'astro/config';
 import db from "@astrojs/db";
-
 import tailwind from "@astrojs/tailwind";
+
+import vue from "@astrojs/vue";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [db(), tailwind()]
+  integrations: [db(), tailwind({
+    applyBaseStyles: false
+  }), vue()]
 });
